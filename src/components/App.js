494c6dbc2e9b7e5fd5fc29bada0752d70b2469b4
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-03-01 12:26:10 +0530

    added about section

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import { useState } from 'react';
 import ArticleList from './ArticleList';
+import About from './About';
 import  DarkMode from './DarkMode';
 import Option from './Option'
 import ArticleListSkeleton from './ArticleListSkeleton';
@@ -20,9 +20,10 @@ function App() {
     <div id='App'>
       <div className="transition-colors delay-200 min-h-screen flex items-center flex-col bg-primary text-center dark:bg-accent-2">
           <h1 className='transition-colors delay-200 text-6xl sm:m-4 m-2 mb-0 font-pop font-bold text-accent-2 dark:text-primary py-3 '>NEWSIFY</h1>
-          <div className='sm:m-5 m-3 sm:gap-x-10 gap-x-5 transition-colors delay-200 flex items-center justify-center w-max gap-2 flex-row'>
+          <div className='sm:m-5 m-3 sm:gap-x-10 gap-x-5 gap-y-5 transition-colors delay-200 flex flex-wrap sm:flex-nowrap  text-center items-center justify-centersm:w-max gap-2 flex-row'>
             <DarkMode/>
             <Option news_channels={news_channels} selected={selected} setSelected={setSelected} />    
+           <About/>
           </div>
           <ArticleListSkeleton isLoaded={isLoaded}  /> 
           <ArticleList news={selected} isLoaded={isLoaded} setIsLoaded={setIsLoaded} /> 
