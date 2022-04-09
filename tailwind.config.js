8372faa8bd33e97ff257a279ac37a97868126666
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-04-09 01:01:14 -0700

    Made a glasssmorphic background to the About section. Also added a line below the heading for aeshetics.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,40 @@
 module.exports = {
-  mode: 'jit',
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: "jit",
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       keyframes: {
         appear: {
-          '0%': {opacity: 0},
-          '100%': {opacity: 1},
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
       animation: {
-        appear: 'appear 1s ease-in-out forwards',
+        appear: "appear 1s ease-in-out forwards",
       },
     },
-    colors:{
-      'primary': '#EEEDDE',
-      'secondary': '#E0DDAA',
-      'accent-1': '#203239',
-      'accent-2': '#141E27',
-      'secondary-shade': '#E0DDAAaa',
-      'accent-shade': '#203239aa',
-      'transparent': 'transparent',
-      'current': 'currentColor',
-
+    colors: {
+      primary: "#EEEDDE",
+      secondary: "#E0DDAA",
+      "accent-1": "#203239",
+      "accent-2": "#141E27",
+      "secondary-shade": "#E0DDAAaa",
+      "accent-shade": "#203239aa",
+      transparent: "transparent",
+      current: "currentColor",
     },
     fontFamily: {
-        pop : ['Poppins', 'sans-serif'],
-        mono : ['Roboto\\ Mono', 'monospace'],
+      pop: ["Poppins", "sans-serif"],
+      mono: ["Roboto\\ Mono", "monospace"],
     },
-    minWIdth:{
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%',
-    }
+    minWIdth: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
   },
   plugins: [],
-}
+};
